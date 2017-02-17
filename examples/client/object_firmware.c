a43d99b6744b8c5797ae15783e175b120044163a
Riccardo Pozza <r.pozza@surrey.ac.uk>
2017-02-17 12:36:37 +0000

    Updated To Current FW 1.0

--- examples/client/object_firmware.c
+++ examples/client/object_firmware.c
@@ -64,7 +64,7 @@
 #define RESULT_BUSY						0
 #define RESULT_UPDATED					1
 
-#define FW_VERSION						"V0.1"
+#define FW_VERSION						"V1.0"
 #define FW_PACKAGE						"Deskegg 1.0"
 #define FW_VER_ADDR						0x80000 //after 512K
 #define FW_STOP_ADDR					0x90000
